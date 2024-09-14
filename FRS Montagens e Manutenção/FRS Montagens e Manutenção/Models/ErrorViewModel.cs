a68c6641d0b582f77377f84a6b334c09cbac59ef
KaikAp <kaik.silva4@outlook.com>
2024-09-14 13:38:59 -0300

    feat: Adicionado layout e pagina index

---
+++ FRS Montagens e Manutenção/FRS Montagens e Manutenção/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FRS_Montagens_e_Manutenção.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
