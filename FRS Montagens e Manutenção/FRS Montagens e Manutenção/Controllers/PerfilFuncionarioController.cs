eb05a66384993fda1883880ea9bc76775833b049
BrunoCalera <brunocalerasp@gmail.com>
2024-10-07 17:02:40 -0300

    feat: Criado página Perfil Funcionario e Perfil Cliente

---
+++ FRS Montagens e Manutenção/FRS Montagens e Manutenção/Controllers/PerfilFuncionarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FRS_Montagens_e_Manutenção.Controllers
+{
+    public class PerfilFuncionarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
